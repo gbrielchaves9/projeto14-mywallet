bbf5d6c10169f36ea4903d9f7898dd9db3eb9a10
Gabriel Chaves <gbrielchaves9@gmail.com>
2023-04-19 17:36:20 -0300

    validacoes seguindo aula

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
-import Joi from "joi";
+import bcrypt from "bcrypt"
+import joi from "joi";
 
 
 const app = express()
 
+const validaShema = joi.object({
+    nome: joi.string().required(),
+    email: joi.string().required(),
+    senha: joi.string().required().min(5)
+})
 app.use(express.json())
 app.use(cors())
 dotenv.config()
@@ -16,3 +22,48 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL)
 mongoClient.connect()
     .then(() => db = mongoClient.db())
     .catch((err) => console.log(err.message))
+
+app.post("/cadastro", async (req, res) => {
+    const { nome, email, senha } = req.body
+
+    const vali = validaShema.validate(req.body, { abortEarly: false })
+
+    if (vali.error) {
+        const errorn = vali.errorn.detals.map((detail) => detail.message)
+        return res.status(422).send(errorn);
+    }
+
+    console.log(req.body)
+    try {
+        const usuario = await db.collection("usuario").findOne({ email })
+        if (usuario) return res.status(401).send("Email usuario ja cadastrado !")
+        const hash = bcrypt.hashSync(senha, 10)
+
+
+        await db.collection("usuario").insertOne({ nome, email, senha: hash })
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/login", async (req, res) => {
+    const { email, senha } = req.body
+    console.log(req.body)
+
+    try {
+        const usuario = await db.collection("usuario").findOne({ email })
+        if (!usuario) return res.status(401).send("Email incorreto!")
+
+        const verificaSenha = bcrypt.compareSync(senha, usuario.senha)
+        if (!verificaSenha) return res.status(401).send("Senha incorreta!")
+
+        res.sendStatus(200)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+const PORT = 5000
+app.listen(PORT, () => console.log("Server listening on port 5000"))
