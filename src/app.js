5010e5d8053adfdaf39b2f06795bb36a4ebf3ca1
Gabriel Chaves <gbrielchaves9@gmail.com>
2023-04-19 17:46:46 -0300

    validacao back ok

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const app = express()
 
 const validaShema = joi.object({
     nome: joi.string().required(),
-    email: joi.string().required(),
-    senha: joi.string().required().min(5)
+    email: joi.string().email().required(),
+    senha: joi.string().required().min(3)
 })
 app.use(express.json())
 app.use(cors())
@@ -22,6 +22,9 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL)
 mongoClient.connect()
     .then(() => db = mongoClient.db())
     .catch((err) => console.log(err.message))
+
+
+
 
 app.post("/cadastro", async (req, res) => {
     const { nome, email, senha } = req.body
@@ -36,16 +39,15 @@ app.post("/cadastro", async (req, res) => {
     console.log(req.body)
     try {
         const usuario = await db.collection("usuario").findOne({ email })
-        if (usuario) return res.status(401).send("Email usuario ja cadastrado !")
+        if (usuario) return res.status(409).send("Usuário já cadastrado com este e-mail!")
         const hash = bcrypt.hashSync(senha, 10)
-
-
         await db.collection("usuario").insertOne({ nome, email, senha: hash })
         res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
     }
 })
+
 
 app.post("/login", async (req, res) => {
     const { email, senha } = req.body
